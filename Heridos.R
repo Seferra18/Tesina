c06ef9e05379096de0686bae1186b74469ac5b04
FERRARO Sebastián Mario <FerraroS@sancristobal.com.ar>
2020-03-23 20:35:20 -0300

    Mejora de codigo y grafico de dispersion para el ebi

--- Heridos.R
+++ Heridos.R
@@ -1,15 +1,17 @@
-#Probando nuevas cosas - Tesina
-#despu閟 borrar todas las librer韆s q siguen despu閟
+# Heridos de arma de fuego
 list.of.packages <- c("readxl","ggplot2","sp","rgdal","RColorBrewer","Matrix","lattice","classInt","gtools","sf","spdep","dplyr", "xlsx", "lubridate", "tidyverse","car","raster","rgeos")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
 if(length(new.packages)) install.packages(new.packages)
 for (paquete in list.of.packages) {suppressMessages(library(paquete,character.only = TRUE) ) }
-#Poligonos
-rosmap <- readOGR("C:/Users/FerraroS/Desktop/Quinto a駉 - Tesis/Bases", "Rosario2010")
-#Conjunto de datos con info equivalente
-df <- read_excel("C:/Users/FerraroS/Desktop/Quinto a駉 - Tesis/Bases/prohibido_1.xlsx")
+
+# Poligonos
+rosmap <- readOGR("C:/Users/FerraroS/Desktop/Todo_seba/Cosas/Quinto anio - Tesis/Bases", "Rosario2010")
+
+# Conjunto de datos con info de heridos
+df <- read_excel("C:/Users/FerraroS/Desktop/Todo_seba/Cosas/Quinto anio - Tesis/Bases/prohibido_1.xlsx")
 prohibido <- df
-#Se detectan registros duplicados
+
+# Se detectan registros duplicados
 coordinates(df) <- ~X+Y
 duplicados <- zerodist(df)
 for (i in 1:nrow(duplicados)){
@@ -18,34 +20,40 @@ for (i in 1:nrow(duplicados)){
 for (i in 1:nrow(duplicados)){
   prohibido <- prohibido[prohibido$POLY_ID != duplicados[i, 1],]
 }
-#Es necesario eliminar la observacion 842, ya que no contiene hogares
+
+# Es necesario eliminar la observacion 842, ya que no contiene hogares
 rosmap <- rosmap[rosmap$POLY_ID != 842, ]
-#Dataset heridos
+
+# Dataset heridos
 prohibido <- filter(prohibido, !POLY_ID == 842)
-#join entre ambos dataframes
+
+# join entre ambos dataframes
 heridos <- sp::merge(rosmap, prohibido, by = c("POLY_ID" = "POLY_ID"))
 rosmap <- heridos
-#heridos@data = heridos@data[!heridos@data$Heridos== 0, ]
+
+# heridos@data = heridos@data[!heridos@data$Heridos== 0, ]
 x <- rosmap$Proporcion #Variable de interes principal
 arrow <- list("SpatialPolygonsRescale", layout.north.arrow(), offset=c(-60.75, -33), scale(10))
-spplot(rosmap, "porc", key.space = list(x = 0.62, y = 1, corner = c(0, 1)), sp.layout = list(arrow),
+spplot(rosmap, "Proporcion", key.space = list(x = 0.62, y = 1, corner = c(0, 1)), sp.layout = list(arrow),
        colorkey = list(space = "right"), scales = list(draw = T),
-       main = "Porcentaje de Hogares con NBI en Rosario", as.table = TRUE)
-#Definici髇 del vecindario tipo reina
+       main = "Porcentaje de heridos de arma de fuego en Rosario", as.table = TRUE)
+
+# Definicion del vecindario tipo reina
 reina <- poly2nb(rosmap, queen = TRUE)
 lista <- nb2listw(reina, style = "S")
-moran.test(x, lista)#0.38
-moran.mc(x, lista, nsim = 999)#bajo los supuestos del test permutacional
-#distribucion del numero de vecinos
+moran.test(x, lista) # Supuesto de normalidad
+moran.mc(x, lista, nsim = 999)# Test permutacional
+
+# Distribucion del numero de vecinos
 vecinos <- table(card(reina))
 vecinos <- as.data.frame(vecinos)
 names(vecinos)[1] = "Vecinos"
 names(vecinos)[2] = "Frecuencia"
 ggplot(vecinos, aes(x = Vecinos, y = Frecuencia)) +
   geom_bar(color = "blue", stat = "identity") +
-  scale_x_discrete("N鷐ero de Vecinos") +
+  scale_x_discrete("N煤mero de Vecinos") +
   scale_y_continuous("Frecuencia") +
-  ggtitle("Distribuci髇 del NUmero de Vecinos") +
+  ggtitle("Distribuci贸n del Numero de Vecinos") +
   theme(
     plot.title = element_blank()
   )
@@ -57,28 +65,25 @@ names(rezagos)[2] = "Retardos"
 ggplot(rezagos, aes(x = Hogares, y = Retardos)) +
   geom_point(color = "blue", size = 2, shape = 20) +
   stat_smooth(method = "lm", se = F, col = "red") +
-  scale_x_continuous("Proporci髇 de Hogares con NBI") +
+  scale_x_continuous("Proporci贸n de Heridos con arma de fuego") +
   scale_y_continuous("Retardo Espacial") +
-  ggtitle("Gr醘ico de dispersi髇 de Moran") +
+  ggtitle("Gr谩fico de dispersi贸n de Moran") +
   theme(
     plot.title = element_blank()
   )
 #Obtencion de los centroides de cada radio censal
-centroides <- gCentroid(rosmap, byid = TRUE)
-plot(centroides)
+#centroides <- gCentroid(rosmap, byid = TRUE)
+#plot(centroides)
 #ggsave() para guardar el grafico ggsave("miDibujito.jpg", g,width = 8, height = 5, units = "cm") puede ser pdf, etc
 #hay opciones para llevarlo a powerpoint y editarlo alli
 #ver ggfortify
 #Obtencion de la matriz de pesos W
 W <- nb2mat(reina, glist = NULL, style = "S", zero.policy = NULL)
-#C醠culo del 韓dice usual utilizando las matrices 
+#Calculo del indice de Moran utilizando las matrices 
 #Se comprueba la igualdad con el obtenido directamente con la sentencia correspondiente
-ni <-rosmap$Heridos#vector que contiene el n鷐ero de hogares con nbi en cada radio censal
-#View(ni)
-m <- length(ni)#n鷐ero de 醨eas o radios censales
-#View(m)
-xi <- rosmap$THOGARES.x# vector que contiene el n鷐ero de hogares en cada radio censal
-#View(xi)
+ni <-rosmap$Heridos#vector que contiene el n?mero de hogares con nbi en cada radio censal
+m <- length(ni)#n?mero de ?reas o radios censales
+xi <- rosmap$THOGARES.x# vector que contiene el n?mero de hogares en cada radio censal
 pi <- ni / xi
 pmedia <- sum(pi) / m
 pdif <- pi - pmedia
@@ -88,31 +93,41 @@ Imoran <- numerador / denominador
 print(Imoran)#Coincide con el calculado anteriormente
 #EBI: Empirical Bayes Index
 Q <- EBest(ni, xi, family = "poisson")
-View(Q)#La primer columna contiene los pi y la segunda las estimaciones emp韗icas de bayes
-#C醠culo de EBI
-ebi <- EBImoran.mc(ni, xi, lista, nsim = 999, zero.policy = TRUE)#observamos que se incrementa el valor del 韓dice con respecto al de referencia
+View(Q)#La primer columna contiene los pi y la segunda las estimaciones emp?ricas de bayes
+#C?lculo de EBI
+ebi <- EBImoran.mc(ni, xi, lista, nsim = 999, zero.policy = TRUE)#observamos que se incrementa el valor del ?ndice con respecto al de referencia
 print(ebi)
 summary(ebi)
 View(ebi)
 plot(ebi)#----Pensar los graficos que se pueden hacer
-#obtenci髇 de la matriz de pesos M
+# Grafico mejorado para el EBI
+rezagos <- Q
+rezagos <- as.data.frame(rezagos)
+names(rezagos)[1] = "Hogares"
+names(rezagos)[2] = "Retardos"
+ggplot(rezagos, aes(x = Hogares, y = Retardos)) +
+  geom_point(color = "blue", size = 2, shape = 20) +
+  stat_smooth(method = "lm", se = F, col = "red") +
+  scale_x_continuous("Proporci贸n de Heridos de arma de fuego") +
+  scale_y_continuous("Retardo Espacial") +
+  coord_cartesian(xlim = c(0, 0.05), ylim = c(0, 0.05)) +
+  ggtitle("Gr谩fico de dispersi贸n de Moran") +
+  theme(
+    plot.title = element_blank()
+  )
+
+# Obtencion de la matriz de pesos M
 M <- W
-diag(M) <- 2#Para todo i=j--->mij=2, tal como se realiza en el estudio
-#View(M)
-n <- sum(ni)#n鷐ero totales de hogares con nbi en los 1072 radios censales
-#View(n)
-x <- sum(xi)#n鷐ero total de hogares en todos los radios censales
-#View(x)
-b <- n/x #Proporci髇 de hogares con nbi. Distinto al propuesto por assuncao, pero esta bien programado (ver Oden)
-#View(b)
-ei <- ni/n#Vector que contiene la proporci髇 que representa cada radio censal con respecto al total de hogares con nbi
-#View(ei)
-di <- xi/x#Vector que contiene la proporci髇 que representa cada radio censal con respecto al total de hogares
-#View(di)
-ed <- ei - di#Vector que contiene la diferencias de proporciones definidas anteriormente
-#View(ed)
-#se define Mij*
+diag(M) <- 2 #Para todo i=j--->mij=2, tal como se detalla en el paper
+n <- sum(ni) #numero totales de hogares con nbi en todos los radios censales
+x <- sum(xi) #numero total de hogares en todos los radios censales
+b <- n/x #Proporcion de hogares con nbi. Distinto al propuesto por assuncao, pero esta bien programado (ver Oden)
+ei <- ni/n #Vector que contiene la proporcion que representa cada radio censal con respecto al total de hogares con nbi
+di <- xi/x #Vector que contiene la proporcion que representa cada radio censal con respecto al total de hogares
+ed <- ei - di #Vector que contiene la diferencias entre las proporciones definidas anteriormente
+# Se define Mij*
 peso <- matrix(rep(sqrt(1 / di), m), ncol = m)*t(matrix(rep(sqrt(1 / di), m), ncol=m)) * M
+# Matrices que contienen calculos parciales con el fin de facilitar el calculo
 A <- sum(matrix(rep(di, m), ncol = m)*t(matrix(rep(di, m), ncol = m)) * peso)
 B <- sum(di * diag(peso))
 C <- sum(matrix(rep(di, m), ncol = m) * t(matrix(rep(di, m), ncol = m)) * (4 * peso ^ 2))
@@ -122,19 +137,19 @@ FI <- sum(di * diag(peso) * (2 * di %*% peso))
 s0 <- x ^ 2 * A - x * B
 s1 <- (x ^ 2 * C) / 2 - 2 * x * D
 s2 <- x ^ 3 * E - 4 * x ^ 2 * FI + 4 * x * D
+# Numerador y denominador del indice de Oden
 num <- n ^ 2 * (ed %*% (peso %*% ed)) - n * (1 - 2 * b) * sum(ei * diag(peso)) - n * b * sum(di * diag(peso))
 den <- (b * (1 - b) * s0)
+# Indice de Oden
 Ipop <- num / den
-print(Ipop)
+# Esperanza y variancia
 b2 <- 1 / (b * (1 - b)) - 3
 aux <- x * ((x ^ 2 - 3 * x + 3) * s1 - x * s2 + 3 * s0 ^ 2) - b2 * (x * (x - 1) * s1 - 2 * x *s2 + 6 * s0 ^ 2)
 espcuad <- aux / ((x - 1) * (x - 2) * (x - 3) * s0 ^ 2)
 stdev <- sqrt(espcuad - 1 / (x - 1) ^ 2)
 z <- (Ipop + 1 / (x - 1)) / stdev
-var_aprox <- (2 * A ^ 2 + C /2 - E) / (A ^ 2 * x ^ 2)#V醠ido para x grande
-resultados <-c (Ipop, -1 / (x - 1), stdev ^ 2, var_aprox, stdev, z, 1 - pnorm(z))#pnorm indica la funci髇n de distribuci髇 normal, por lo tanto 1-pnorm(Zdesvio) es el p-valor
-etiquetas <- c("I*pop", "Media", "Variancia", "Variancia Aproximada", "Desv韔 Est醤dar", "Z", "P-valor")
+var_aprox <- (2 * A ^ 2 + C /2 - E) / (A ^ 2 * x ^ 2) #A proximacion valida para x grande
+resultados <-c (Ipop, -1 / (x - 1), stdev ^ 2, var_aprox, stdev, z, 1 - pnorm(z)) #pnorm indica la funcionn de distribucion normal, por lo tanto 1-pnorm(Zdesvio) es el p-valor
+etiquetas <- c("I*pop", "Media", "Variancia", "Variancia Aproximada", "Desvio Estandar", "Z", "P-valor")
 resultados_finales <- cbind(etiquetas, resultados)#concatena los vectores por columna
 View(resultados_finales)
-
-
--- NBI.R
+++ NBI.R
@@ -1,43 +1,56 @@
-#Probando nuevas cosas - Tesina
+# Carga de paquetes necesarios 
 list.of.packages <- c("readxl","ggplot2","sp","rgdal","RColorBrewer","Matrix","lattice","classInt","gtools","sf","spdep","dplyr", "xlsx", "lubridate", "tidyverse","car","raster","rgeos")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[, "Package"])]
 if(length(new.packages)) install.packages(new.packages)
 for (paquete in list.of.packages) {suppressMessages(library(paquete,character.only = TRUE) ) }
-#Poligonos
-rosmap <- readOGR("C:/Users/FerraroS/Desktop/Todo_seba/Cosas/Quinto año - Tesis/Bases", "Rosario2010")
-#Conjunto de datos con info equivalente
-df <- read_excel("C:/Users/FerraroS/Desktop/Todo_seba/Cosas/Quinto año - Tesis/Bases/Datos_2010.xlsx")
+
+#Poligonos correspondientes a los radios censales de Rosario
+rosmap <- readOGR("C:/Users/FerraroS/Desktop/Todo_seba/Cosas/Quinto anio - Tesis/Bases", "Rosario2010")
+
+# df con los centroides de los radios censales
+df <- read_excel("C:/Users/FerraroS/Desktop/Todo_seba/Cosas/Quinto anio - Tesis/Bases/Datos_2010.xlsx")
+
 #Grafico antes de pasarlo a la clase spatyal points dataframe
-ggplot(df, aes(X, Y)) +
-  geom_point()
-#Se detectan registros duplicados
+#ggplot(df, aes(X, Y)) +
+#  geom_point()
+
+# Se indica que X e Y son las latitudes y longitudes correspondientes
 coordinates(df) <- ~X+Y
+
+#Se detectan registros duplicados
 duplicados <- zerodist(df)
+
+# Se elimina uno de los duplicados
 for (i in 1:nrow(duplicados)){
   rosmap <- rosmap[rosmap$POLY_ID != duplicados[i, 1],]
-  }
+}
+
 #Es necesario eliminar la observacion 842, ya que no contiene hogares
 rosmap <- rosmap[rosmap$POLY_ID != 842, ]
+
 x <- rosmap$prop #Variable de interes principal
-#EDA
+
+# Grafico exploratorio
 plot(rosmap, main="Radios censales de la ciudad de Rosario")
 
-#ggplot(rosmap) + 
- # geom_map(aes(map_id = POLY_ID), map = positions)
+# Grafico de calor que indica el porcentaje de hogares con NBI a lo largo de todos los radios censales en Rosario
 arrow <- list("SpatialPolygonsRescale", layout.north.arrow(), offset=c(-60.75, -33), scale(10))
+
 spplot(rosmap, "porc", key.space = list(x = 0.62, y = 1, corner = c(0, 1)), sp.layout = list(arrow),
        colorkey = list(space = "right"), scales = list(draw = T),
        main = "Porcentaje de Hogares con NBI en Rosario", as.table = TRUE)
-#Definici?n del vecindario tipo reina
+
+#Definicion del vecindario tipo reina
 reina <- poly2nb(rosmap, queen = TRUE)#dnearneigh basado en distancias
+
+# Se escoge el metodo de estandarizacion de filas para la matriz de pesos espaciales
 lista <- nb2listw(reina, style = "S")
-moran.test(x, lista)#0.38
-moran.mc(x, lista, nsim = 999)#bajo los supuestos del test permutacional
-#graficos
-# ggplot(Datos_2010) +
-#   geom_map(aes(map_id = rosmap$POLY_ID), map = rosmap) +
-#   expand_limits(rosmap)
-#distribucion del numero de vecinos
+
+# Indice de Moran
+moran.test(x, lista)# Supuesto de normalidad
+moran.mc(x, lista, nsim = 999) # Test permutacional
+
+# Distribucion del numero de vecinos
 vecinos <- table(card(reina))
 vecinos <- as.data.frame(vecinos)
 names(vecinos)[1] = "Vecinos"
@@ -46,11 +59,12 @@ ggplot(vecinos, aes(x = Vecinos, y = Frecuencia)) +
   geom_bar(color = "blue", stat = "identity") +
   scale_x_discrete("Número de Vecinos") +
   scale_y_continuous("Frecuencia") +
-  ggtitle("Distribución del NUmero de Vecinos") +
+  ggtitle("Distribución del Número de Vecinos") +
   theme(
     plot.title = element_blank()
   )
-#grafico de dispersion de moran
+
+# Grafico de dispersion de moran
 rezagos <- cbind(x, lag.listw(lista, x))
 rezagos <- as.data.frame(rezagos)
 names(rezagos)[1] = "Hogares"
@@ -58,65 +72,77 @@ names(rezagos)[2] = "Retardos"
 ggplot(rezagos, aes(x = Hogares, y = Retardos)) +
   geom_point(color = "blue", size = 2, shape = 20) +
   stat_smooth(method = "lm", se = F, col = "red") +
-  scale_x_continuous("Proporci?n de Hogares con NBI") +
+  scale_x_continuous("Proporción de Hogares con NBI") +
   scale_y_continuous("Retardo Espacial") +
   coord_cartesian(xlim = c(0, 1), ylim = c(0, 0.4)) +
-  ggtitle("Gr?fico de dispersi?n de Moran") +
+  ggtitle("Gráfico de dispersión de Moran") +
   theme(
     plot.title = element_blank()
     )
-#Obtencion de los centroides de cada radio censal
+
+# Obtencion de los centroides de cada radio censal
 centroides <- gCentroid(rosmap, byid = TRUE)
 plot(centroides)
+
 #ggsave() para guardar el grafico ggsave("miDibujito.jpg", g,width = 8, height = 5, units = "cm") puede ser pdf, etc
 #hay opciones para llevarlo a powerpoint y editarlo alli
 #ver ggfortify
 
-#Obtencion de la matriz de pesos W
+# Obtencion de la matriz de pesos W
 W <- nb2mat(reina, glist = NULL, style = "S", zero.policy = NULL)
-#C?lculo del ?ndice usual utilizando las matrices 
+
+# Calculo del indice de Moran utilizando las matrices 
 #Se comprueba la igualdad con el obtenido directamente con la sentencia correspondiente
-ni <-rosmap$con_NBI#vector que contiene el n?mero de hogares con nbi en cada radio censal
-#View(ni)
-m <- length(ni)#n?mero de ?reas o radios censales
-#View(m)
-xi <- rosmap$THOGARES# vector que contiene el n?mero de hogares en cada radio censal
-#View(xi)
+ni <-rosmap$con_NBI #vector que contiene el numero de hogares con nbi en cada radio censal
+m <- length(ni) #numero de areas o radios censales
+xi <- rosmap$THOGARES #vector que contiene el numero de hogares en cada radio censal
 pi <- ni / xi
 pmedia <- sum(pi) / m
 pdif <- pi - pmedia
 denominador <- sum(W) * (t(pdif) %*% pdif)
 numerador <- m * (t(pdif) %*% W %*% pdif)
 Imoran <- numerador / denominador
-print(Imoran)#Coincide con el calculado anteriormente
+print(Imoran) #Coincide con el calculado anteriormente
+
 #EBI: Empirical Bayes Index
 Q <- EBest(ni, xi, family = "poisson")
-View(Q)#La primer columna contiene los pi y la segunda las estimaciones emp?ricas de bayes
-#C?lculo de EBI
-ebi <- EBImoran.mc(ni, xi, lista, nsim = 999, zero.policy = TRUE)#observamos que se incrementa el valor del ?ndice con respecto al de referencia
+View(Q)#La primer columna contiene los pi y la segunda las estimaciones empiricas de bayes
+# Calculo del EBI
+ebi <- EBImoran.mc(ni, xi, lista, nsim = 999, zero.policy = TRUE) #observamos que se incrementa el valor del indice con respecto al de Moran
 print(ebi)
 summary(ebi)
 View(ebi)
 plot(ebi)#----Pensar los graficos que se pueden hacer
 plot(Q)
-#obtenci?n de la matriz de pesos M
+
+# Grafico mejorado para el EBI
+rezagos <- Q
+rezagos <- as.data.frame(rezagos)
+names(rezagos)[1] = "Hogares"
+names(rezagos)[2] = "Retardos"
+ggplot(rezagos, aes(x = Hogares, y = Retardos)) +
+  geom_point(color = "blue", size = 2, shape = 20) +
+  stat_smooth(method = "lm", se = F, col = "red") +
+  scale_x_continuous("Proporción de Hogares con NBI") +
+  scale_y_continuous("Retardo Espacial") +
+  coord_cartesian(xlim = c(0, 1), ylim = c(0, 0.8)) +
+  ggtitle("Gráfico de dispersión de Moran") +
+  theme(
+    plot.title = element_blank()
+  )
+
+# Obtencion de la matriz de pesos M
 M <- W
-diag(M) <- 2#Para todo i=j--->mij=2, tal como se realiza en el estudio
-#View(M)
-n <- sum(ni)#n?mero totales de hogares con nbi en los 1072 radios censales
-#View(n)
-x <- sum(xi)#n?mero total de hogares en todos los radios censales
-#View(x)
-b <- n/x #Proporci?n de hogares con nbi. Distinto al propuesto por assuncao, pero esta bien programado (ver Oden)
-#View(b)
-ei <- ni/n#Vector que contiene la proporci?n que representa cada radio censal con respecto al total de hogares con nbi
-#View(ei)
-di <- xi/x#Vector que contiene la proporci?n que representa cada radio censal con respecto al total de hogares
-#View(di)
-ed <- ei - di#Vector que contiene la diferencias de proporciones definidas anteriormente
-#View(ed)
-#se define Mij*
+diag(M) <- 2 #Para todo i=j--->mij=2, tal como se detalla en el paper
+n <- sum(ni) #numero totales de hogares con nbi en todos los radios censales
+x <- sum(xi) #numero total de hogares en todos los radios censales
+b <- n/x #Proporcion de hogares con nbi. Distinto al propuesto por assuncao, pero esta bien programado (ver Oden)
+ei <- ni/n #Vector que contiene la proporcion que representa cada radio censal con respecto al total de hogares con nbi
+di <- xi/x #Vector que contiene la proporcion que representa cada radio censal con respecto al total de hogares
+ed <- ei - di #Vector que contiene la diferencias entre las proporciones definidas anteriormente
+# Se define Mij*
 peso <- matrix(rep(sqrt(1 / di), m), ncol = m)*t(matrix(rep(sqrt(1 / di), m), ncol=m)) * M
+# Matrices que contienen calculos parciales con el fin de facilitar el calculo
 A <- sum(matrix(rep(di, m), ncol = m)*t(matrix(rep(di, m), ncol = m)) * peso)
 B <- sum(di * diag(peso))
 C <- sum(matrix(rep(di, m), ncol = m) * t(matrix(rep(di, m), ncol = m)) * (4 * peso ^ 2))
@@ -126,41 +152,27 @@ FI <- sum(di * diag(peso) * (2 * di %*% peso))
 s0 <- x ^ 2 * A - x * B
 s1 <- (x ^ 2 * C) / 2 - 2 * x * D
 s2 <- x ^ 3 * E - 4 * x ^ 2 * FI + 4 * x * D
+# Numerador y denominador del indice de Oden
 num <- n ^ 2 * (ed %*% (peso %*% ed)) - n * (1 - 2 * b) * sum(ei * diag(peso)) - n * b * sum(di * diag(peso))
 den <- (b * (1 - b) * s0)
+# Indice de Oden
 Ipop <- num / den
-print(Ipop)
+# Esperanza y variancia
 b2 <- 1 / (b * (1 - b)) - 3
 aux <- x * ((x ^ 2 - 3 * x + 3) * s1 - x * s2 + 3 * s0 ^ 2) - b2 * (x * (x - 1) * s1 - 2 * x *s2 + 6 * s0 ^ 2)
 espcuad <- aux / ((x - 1) * (x - 2) * (x - 3) * s0 ^ 2)
 stdev <- sqrt(espcuad - 1 / (x - 1) ^ 2)
 z <- (Ipop + 1 / (x - 1)) / stdev
-var_aprox <- (2 * A ^ 2 + C /2 - E) / (A ^ 2 * x ^ 2)#V?lido para x grande
-resultados <-c (Ipop, -1 / (x - 1), stdev ^ 2, var_aprox, stdev, z, 1 - pnorm(z))#pnorm indica la funci?nn de distribuci?n normal, por lo tanto 1-pnorm(Zdesvio) es el p-valor
-etiquetas <- c("I*pop", "Media", "Variancia", "Variancia Aproximada", "Desv?o Est?ndar", "Z", "P-valor")
+var_aprox <- (2 * A ^ 2 + C /2 - E) / (A ^ 2 * x ^ 2) #A proximacion valida para x grande
+resultados <-c (Ipop, -1 / (x - 1), stdev ^ 2, var_aprox, stdev, z, 1 - pnorm(z)) #pnorm indica la funcionn de distribucion normal, por lo tanto 1-pnorm(Zdesvio) es el p-valor
+etiquetas <- c("I*pop", "Media", "Variancia", "Variancia Aproximada", "Desvio Estandar", "Z", "P-valor")
 resultados_finales <- cbind(etiquetas, resultados)#concatena los vectores por columna
 View(resultados_finales)
 
 
 
-
-
-
-
-
-
-
-
-
-#sp.correlogram(listw, x, order = 1, method = "corr", style = "B")
-#Extras
-#dnearneigh vecinos con criterio de distancia euclidea--> Siniestralidad
-probmap(ros_im$THOGARES, ros_im$con_NBI)#investigar m?s para que puede ser ?til
-plot.skater(ros_im$Distrito, xy)
-#Obtencion de la matriz de pesos W
-W <- nb2mat(nb, glist = NULL, style = "B", zero.policy = NULL)
-
-#Example
+seba <- st_read(system.file("shapes/Rosario2010.shp", package="spData")[1], quiet=TRUE)
+# Ver, podria ser util
 columbus <- st_read(system.file("shapes/columbus.shp", package="spData")[1], quiet=TRUE)
 col.gal.nb <- read.gal(system.file("weights/columbus.gal", package="spData")[1])
 coords <- st_coordinates(st_centroid(st_geometry(columbus)))
@@ -197,6 +209,3 @@ system.time(xxnb <- poly2nb(as(nc.sids, "Spatial")))
 plot(st_geometry(nc.sids))
 plot(xxnb, coordinates(as(nc.sids, "Spatial")), add=TRUE, col="blue")
 
-#FunciÃ³n para los centroides
-#cents <- SpatialPointsDataFrame(coords=rosmap, data=rosmap@data, 
- #                               proj4string=rosmap@proj4string)
